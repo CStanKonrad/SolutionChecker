6e4779c9aa607f7331adbcd713ec7fb10ba0aa3a
CStanKonrad <CStanKonrad@users.noreply.github.com>
2016-02-19 16:48:50 +0100

    better zip fix

--- src/Checker.cpp
+++ src/Checker.cpp
@@ -207,7 +207,7 @@ void generateAnswers(const SSettings &_settings)
 				break;
             if (checkResult.solutionReturnVal != int(checkResult.ESolRet::OK) && _settings.noClearStop == true)
                 break;
-            std::cout << (std::string("zip -j \"") + fullPath + _settings.testSubFolder + _settings.solutionName + std::string("\" \"") + fullPath + _settings.testSubFolder +  fileNameBuf.out + std::string("\"")) << std::endl;
+            //std::cout << (std::string("zip -j \"") + fullPath + _settings.testSubFolder + _settings.solutionName + std::string("\" \"") + fullPath + _settings.testSubFolder +  fileNameBuf.out + std::string("\"")) << std::endl;
             system((std::string("zip -j \"") + fullPath + _settings.testSubFolder + _settings.solutionName + std::string("\" \"") + fullPath + _settings.testSubFolder +  fileNameBuf.out + std::string("\"")).c_str());
         }
         else if (isSuffix(fileNameBuf.in, std::string(".test")) == true)
